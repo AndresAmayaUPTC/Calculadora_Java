dd0fcf63d5b679f4b222b36a0684d281cd93e262
Andres Amaya <andres.amaya03@uptc.edu.co>
2023-08-06 16:44:14 -0500

    Stack creada

--- src/co/edu/uptc/runner/Main.java
+++ src/co/edu/uptc/runner/Main.java
@@ -1,8 +1,26 @@
 package co.edu.uptc.runner;
 
+import co.edu.uptc.dataS.MyStack;
+
 public class Main {
-
     public static void main(String[] args) {
+        MyStack<Integer> myStack = new MyStack<>();
 
+        myStack.push(10);
+        myStack.push(20);
+        myStack.push(30);
+
+        System.out.println("Elementos en la pila:");
+        myStack.printStack();
+
+        System.out.println("Tamaño de la pila: " + myStack.size());
+
+        System.out.println("Elemento en la parte superior de la pila: " + myStack.peek());
+
+        System.out.println("Elementos eliminados de la pila:");
+        while (!myStack.isEmpty()) {
+            System.out.println(myStack.pop());
+        }
     }
-}
+    }
+
